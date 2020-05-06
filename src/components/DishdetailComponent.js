0188b91285bbff922bf7c03b9000aca477b99dde
Suma Sree <suma999sree@gmail.com>
2020-05-06 13:12:50 +0530

    presentational and container Components

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {Component} from 'react';
+import {Card,CardBody,CardImg,CardImgOverlay,CardTitle,CardText} from 'reactstrap';
+class DishDetail extends Component{
+	constructor(props)
+	{
+		super(props);
+		this.state={
+            
+		}
+		
+	}
+	renderDish(dish)
+	{
+		if( dish != null)
+		{   return(
+
+			<div className="row">
+			<div className="col-12 col-md-5">
+			<Card>
+			  <CardImg width="100%" src={dish.image} alt={dish.name} />
+			  <CardBody>
+			  <CardTitle>{dish.name}</CardTitle>
+			  <CardText>{dish.description}</CardText>
+			  </CardBody>
+			</Card>
+			</div>
+			
+			{this.renderComments(dish.comments)}
+			
+			</div>
+			 );
+		}
+		else
+		{
+            return(
+            <div></div>
+            );
+		}
+	}
+	renderComments(comments)
+	{   if(comments.length != 0){
+		   const getcomments=comments.map((commentin)=>{
+		   	return(
+		   
+		   	<div key={commentin.id}>
+		   	<li>
+		   	  {commentin.comment}
+		   	  <br/>
+		   	  <p>--{commentin.author},{new Intl.DateTimeFormat('en-US',{year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(commentin.date)))}</p>
+		   	  </li>
+		   	</div>
+		   	
+		   	);
+		   });
+           return(
+              
+             <div className="col-12 col-md-5 ml-4">
+             <h4>Comments</h4>
+                <ul className="list-unstyled">
+                {getcomments}
+                </ul>
+            
+             </div>
+           );
+		   
+        }
+         else
+         {
+         	return(
+         	<div></div>
+         	);
+         }
+    }
+	render()
+	{
+		return(
+		<div className="container">
+		{this.renderDish(this.props.dish)}
+		</div>
+		);
+	}
+}
+export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,48 +1,24 @@
 import React from 'react';
 import {Component} from 'react';
 import {Card,CardBody,CardImg,CardImgOverlay,CardTitle,CardText} from 'reactstrap';
+
 class Menu extends Component{
 	constructor(props)
 	{
 		super(props);
 		this.state={
 			dishes:this.props.dishes,
-			selectedDish:null
+			
+			
 		};
-	}
-	onDishSelect(dish)
-	{
-		this.setState({selectedDish:dish});
-	}
-	renderDish(dish)
-	{
-		if( dish != null)
-		{   return(
-			<div>
-			<Card>
-			  <CardImg width="100%" src={dish.image} alt={dish.name} />
-			  <CardBody>
-			  <CardTitle>{dish.name}</CardTitle>
-			  <CardText>{dish.description}</CardText>
-			  </CardBody>
-			</Card>
-			</div>
-			 );
-		}
-		else
-		{
-            return(
-            <div></div>
-            );
-		}
 	}
 	render(){
 		const menu=this.state.dishes.map((dish)=>{
                return(
                	 <div key={dish.id} className="col-12 col-md-5 m-1">
-               	   <Card onClick={()=>this.onDishSelect(dish)}>
+               	   <Card onClick={() => this.props.onClick(dish.id) }>
                   
-                      <CardImg width="100%" src={dish.image} alt={dish.name} />
+                     <CardImg width="100%" src={dish.image} alt={dish.name} />
                      <CardImgOverlay><b>{dish.name}</b></CardImgOverlay>
                 
                     </Card>
@@ -54,15 +30,9 @@ class Menu extends Component{
 		return(
 			<div className="container">
 			    <div className="row">
-			       
-			       {menu}
-			       
+			     {menu}
 			    </div>
-			    <div className="row">
-                  <div  className="col-10">
-                    {this.renderDish(this.state.selectedDish)}
-                  </div>
-                </div>
+                 
 			</div>
 			);
 	}
